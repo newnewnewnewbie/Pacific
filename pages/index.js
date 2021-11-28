d6896511f935dd86621d8e525b17b1e3167b6d69
VanSubstance <50601968+VanSubstance@users.noreply.github.com>
2021-11-28 19:29:36 +0900

    [수정] 연습 코드 배제

--- pages/index.js
+++ pages/index.js
@@ -2,11 +2,9 @@ const Index = () => {
   return (
     <div>
       <h1>Wellcome to hell</h1>
-      <p>기본 설정중..</p>
+      <p>기본 설정중.?.</p>
     </div>
   )
 }
 
 export default Index;
-export {default as homeTest} from './homeTest';
-export {default as fullCourseTest} from './fullCourseTest';
