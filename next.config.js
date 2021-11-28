ac8b8d4c1bedd1dddf31e5a14e1939d2e0f2c281
VanSubstance <50601968+VanSubstance@users.noreply.github.com>
2021-11-28 20:53:14 +0900

    [추가] 정적 폴더 관련 설정 진행중
    ! npm i 해야함

--- next.config.js
+++ next.config.js
@@ -1,1 +1,9 @@
-module.exports = {};
+const withImages = require("next-images");
+
+module.exports = withImages({
+  webpack(config, options) {
+    config.resolve.modules.push(__dirname); //절대경로 설정 코드
+
+    return config;
+  },
+});
