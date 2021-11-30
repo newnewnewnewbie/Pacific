9f121a2f808d3d45a00bee1726404536d8cca4b1
newnewnewnewbie <67823201+newnewnewnewbie@users.noreply.github.com>
2021-11-30 23:52:24 +0900

    youtube music 메인화면 작성 중

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,8 @@
 import App, { Container } from 'next/app';
+import Image from 'next/image';
 import Head from 'next/head';
 import React from 'react';
+import img1 from "./img/whale.png"
 
 class Root extends App {
   render () {
@@ -10,8 +12,13 @@ class Root extends App {
           <Head>
               <title>Pacific Webstie</title>
           </Head>
+          <div className="headerCSS">
+            <Image src={img1}></Image>
+            <button>홈</button>
+            <button>둘러보기</button>
+            <button>보관함</button>
+            </div>
           <div>
-              <header>Header</header>
               <main>
                 {/* <BrowserRouter>
                   <Route path="/home" component = {homeTest} />
