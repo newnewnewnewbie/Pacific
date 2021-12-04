95555dca10c47a1e9595baaf56d5232395060657
newnewnewnewbie <67823201+newnewnewnewbie@users.noreply.github.com>
2021-12-04 12:59:18 +0900

    메인 화면 Carousel 구현 중

--- pages/_app.js
+++ pages/_app.js
@@ -1,33 +1,26 @@
 import App, { Container } from 'next/app';
-import Image from 'next/image';
 import Head from 'next/head';
 import React from 'react';
-import img1 from "./img/whale.png"
 
 class Root extends App {
-  render () {
+  render() {
     const { Component, ...other } = this.props;
     return (
       <Container>
-          <Head>
-              <title>Pacific Webstie</title>
-          </Head>
-          <div className="headerCSS">
-            <Image src={img1}></Image>
-            <button>홈</button>
-            <button>둘러보기</button>
-            <button>보관함</button>
-            </div>
-          <div>
-              <main>
-                {/* <BrowserRouter>
-                  <Route path="/home" component = {homeTest} />
-                  <Route path="/course" component = {fullCourseTest} />
-                </BrowserRouter> */}
-                  <Component {...other} />
-              </main>
-              <footer>Footer</footer>
-          </div>
+        <Head>
+          <title>Pacific Webstie</title>
+        </Head>
+        <div className="headerCSS">
+          <button>홈</button>
+          <button>둘러보기</button>
+          <button>보관함</button>
+        </div>
+        <div>
+          <main>
+            <Component {...other} />
+          </main>
+          <footer>Footer</footer>
+        </div>
       </Container>
     );
   };
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,39 @@
 import React from "react";
 import Link from "next/link";
+import { Carousel } from "react-responsive-carousel";
 
 const Index = () => {
   return (
     <div>
-      <h1>Wellcome to hell</h1>
+      <h2>맞춤 믹스: 분위기</h2>
+      <div>
+      <Carousel showArrows={true}>
+                <div>
+                    <img src="unnamed.jpg" />
+                    <p className="legend">Legend 1</p>
+                </div>
+                <div>
+                    <img src="unnamed.jpg" />
+                    <p className="legend">Legend 2</p>
+                </div>
+                <div>
+                    <img src="unnamed.jpg" />
+                    <p className="legend">Legend 3</p>
+                </div>
+                <div>
+                    <img src="unnamed.jpg" />
+                    <p className="legend">Legend 4</p>
+                </div>
+                <div>
+                    <img src="unnamed.jpg" />
+                    <p className="legend">Legend 5</p>
+                </div>
+                <div>
+                    <img src="unnamed.jpg" />
+                    <p className="legend">Legend 6</p>
+                </div>
+            </Carousel>
+      </div>
       <p>기본 설정중..</p>
       <Link href="/test/homeTest">
         <button>homeTest로 이동</button>
